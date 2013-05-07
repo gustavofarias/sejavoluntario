48db168d6a145589acb51d20209085769de76a23
Gustavo Farias <bug.ufrj@gmail.com>
2013-05-06 21:59:06 -0300

    First commit

---
+++ sejavoluntario/apps/users/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class User (models.Model):
+    user = models.ForeignKey(User)
+    photo = models.FileField(upload_to='/image')
+    document = models.CharField(max_length=20)
+    telephone = models.CharField(max_length=20)
+    celphone = models.CharField(max_length=20)
+
+    def __unicode__(self):
+            return u"%s" % self.id
