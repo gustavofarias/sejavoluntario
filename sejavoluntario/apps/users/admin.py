c86190bbcce2f32386e082330f6512601ba04394
Gustavo Farias <bug.ufrj@gmail.com>
2013-05-13 21:02:35 -0300

    criação admin.py

---
+++ sejavoluntario/apps/users/admin.py
@@ -0,0 +1,9 @@
+# coding: utf-8
+#!/usr/bin/env python
+
+from django import forms
+from django.contrib import admin
+
+from sejavoluntario.apps.users import models
+
+admin.site.register(models.User)
--- sejavoluntario/apps/users/views.py
+++ sejavoluntario/apps/users/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from sejavoluntario.apps.users import models
+from sejavoluntario.apps.users.models import User
 from sejavoluntario.apps.users.forms import UserRegistrationForm
 
 def userRegistration(request):
     if request.method == 'POST':
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
-            import ipdb;ipdb.set_trace()
+            user = User()
             return HttpResponseRedirect('/thanks/')
     else:
         form = UserRegistrationForm()
