fcece4236e8c9d04d315a3f7a09183c1c3664cf0
Gustavo Farias <bug.ufrj@gmail.com>
2013-05-14 09:23:00 -0300

    area todo errado

--- sejavoluntario/apps/area/models.py
+++ sejavoluntario/apps/area/models.py
@@ -1,3 +1,17 @@
+# coding: utf-8
+
 from django.db import models
 
-# Create your models here.
+
+class Area (models.Model):
+    type = models.FileField(upload_to='/image')
+    document = models.CharField(max_length=20)
+    telephone = models.CharField(max_length=20)
+    celphone = models.CharField(max_length=20)
+    
+    class Meta:
+        verbose_name_plural = "Areas"
+        verbose_name = "Area"
+
+    def __unicode__(self):
+        return self.area.type
