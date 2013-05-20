0d7f23e5f898ec1e301c92724d0ab5d2701b955d
Gustavo Farias <bug.ufrj@gmail.com>
2013-05-20 18:42:36 -0300

    incluindo area de interesse no form de voluntario

--- sejavoluntario/apps/users/forms.py
+++ sejavoluntario/apps/users/forms.py
@@ -2,26 +2,47 @@
 # coding: utf-8
 
 from django import forms
+from django.forms.widgets import CheckboxSelectMultiple
 from django.contrib.auth.models import User
 from django.db import transaction
-from sejavoluntario.apps.users.models import UserProfile
+from sejavoluntario.apps.users.models import Area
 from sejavoluntario.apps.users.models import Banco
+from sejavoluntario.apps.users.models import UserProfile
+from sejavoluntario.apps.users.models import Voluntario
 
+import logging
 
 class UserRegistrationForm(forms.Form):
+    
+    SEX_CHOICES = (
+        ('M', 'Masculino'),
+        ('F', 'Feminino'),
+    )
+    
     first_name = forms.CharField(max_length=100)
     last_name = forms.CharField(max_length=100)
     email = forms.EmailField()
     password = forms.CharField(widget=forms.PasswordInput())
     repeat_password = forms.CharField(widget=forms.PasswordInput())
+    data_nascimento = forms.DateField()
+    sexo = forms.ChoiceField(choices=SEX_CHOICES)
+    area = forms.MultipleChoiceField(choices=Area.objects.all(),
+                                  widget=CheckboxSelectMultiple,
+                                  label="Área de interesse",)
+    
+    def __init__(self, *args, **kwargs):
+        
+        super(UserRegistrationForm, self).__init__(*args, **kwargs)
+
+        self.fields['area'].choices = [(area.pk, area.name) for area in Area.objects.all()]
+        
     
     def clean_email(self):
-        import ipdb;ipdb.set_trace()
         try:
             User.objects.get(email=self.data.get('email'))
             raise forms.ValidationError(u"E-mail já cadastrado.")
         except User.DoesNotExist:
-            pass
+            logging.info("Email ainda não existe na base de dados")
 
         return self.data.get('email')
     
@@ -36,19 +57,30 @@ class UserRegistrationForm(forms.Form):
     
     def save(self, *args, **kwargs):
         user = None
-        
+
         first_name = self.cleaned_data.get('first_name')
         last_name = self.cleaned_data.get('last_name')
         email = self.cleaned_data.get('email')
         password = self.cleaned_data.get('password')
+        sexo = self.cleaned_data.get('sexo')
+        data_nascimento = self.cleaned_data.get('data_nascimento')
+        area = self.cleaned_data.get('area')
         username = email
         
         with transaction.commit_on_success():
             user = User.objects.create_user(username,email,password)
-            userProfile = UserProfile()
-            userProfile.user = user
-            userProfile.save()
+            user.first_name = first_name
+            user.last_name = last_name
             user.save()
+            
+            voluntario = Voluntario()
+            voluntario.user = user
+            voluntario.sexo = sexo
+            voluntario.nascimento = data_nascimento
+            voluntario.save()
+
+            voluntario.areas = area
+            voluntario.save()
         
         return user
     
